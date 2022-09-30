8272aba106f31388986fc9be669e9ea4df7b9220
Richard Demko <rdemko2332@gmail.com>
2022-09-30 12:00:00 -0400

    Adding perl file for holding genomic to transcript location functionality

---
+++ bin/genomicToTranscriptomic.pl
@@ -0,0 +1,252 @@
+#!/usr/bin/perl
+
+#======================================== DEPENDENCIES =========================================================================================
+use lib "$ENV{GUS_HOME}/lib/perl";
+use strict;
+use Data::Dumper;
+use Getopt::Long;
+use GUS::ObjRelP::DbiDatabase;
+use GUS::Supported::GusConfig;
+use DBI;
+use DBD::Oracle;
+
+#===================================== ARGUMENTS AND SETUP ====================================================================================
+my ($testFile, $gusConfigFile, $sequenceId, $regionFile);
+
+&GetOptions("test_file=s"=> \$testFile,
+            "sequence_id=s"=> \$sequenceId,
+            "region_file=s"=> \$regionFile
+           );
+
+$gusConfigFile = $ENV{GUS_HOME} . "/config/gus.config";
+my $gusConfig = GUS::Supported::GusConfig->new($gusConfigFile);
+
+open(TEST, ">$testFile") or die "Cannot create a test file: $!";
+
+my $db = GUS::ObjRelP::DbiDatabase->new($gusConfig->getDbiDsn(),
+                                         $gusConfig->getDatabaseLogin(),
+                                         $gusConfig->getDatabasePassword(),
+                                         0, 0, 1,
+                                         $gusConfig->getCoreSchemaName()
+    );
+
+my $dbh = $db->getQueryHandle();
+
+#======================================= GENERATING LOCATIONSHIFTS ARRAY  =========================================================================
+
+my $INDEL_QUERY = "select i.location, i.shift
+                      from apidb.indel i
+                      where sample_name = '$sequenceId'";
+
+my $INDEL_QUERY_SH = $dbh->prepare($INDEL_QUERY);
+
+$INDEL_QUERY_SH->execute();
+
+my @locationshifts = ();
+my $counter = 0;
+my $currentShift = 0;
+while (my ($location, $shift) = $INDEL_QUERY_SH->fetchrow_array()) {
+    push ( @{$locationshifts[$counter]}, ($location, $shift + $currentShift));
+    $counter++;
+    $currentShift = $shift + $currentShift;
+}
+#print TEST Dumper(\@locationshifts), "\n";
+#close TEST;
+
+#====================================== GENERATING COORDINATES ARRAY ============================================================================
+
+my $COORDINATE_QUERY = "select listagg(taf.source_id, ',') WITHIN GROUP (ORDER BY taf.aa_feature_id) as transcripts, s.source_id, tf.parent_id as gene_na_feature_id, el.start_min as exon_start, el.end_max as exon_end, decode(el.is_reversed, 1, afe.coding_end, afe.coding_start) as cds_start, decode(el.is_reversed, 1, afe.coding_start, afe.coding_end) as cds_end, el.is_reversed from dots.transcript tf , dots.translatedaafeature taf , dots.aafeatureexon afe , dots.exonfeature ef , dots.nalocation el, dots.nasequence s where tf.na_feature_id = taf.na_feature_id and taf.aa_feature_id = afe.aa_feature_id and afe.exon_feature_id = ef.na_feature_id and ef.na_feature_id = el.na_feature_id  and ef.na_sequence_id = s.na_sequence_id and tf.external_database_release_id = 5981 and s.source_id = 'OU755535' group by s.source_id, tf.parent_id, el.start_min, el.end_max, afe.coding_start, afe.coding_end, el.is_reversed order by s.source_id, el.start_min";
+my $COORDINATE_QUERY_SH = $dbh->prepare($COORDINATE_QUERY);
+$COORDINATE_QUERY_SH->execute();
+my @coordinates = ();
+$counter=0;
+while (my ($transcript, $source_id, $gene_na_feature_id, $exon_start, $exon_end, $cds_start, $cds_end, $rev) = $COORDINATE_QUERY_SH->fetchrow_array()) {
+  push (@{$coordinates[$counter]}, ($cds_start, $cds_end, $rev));
+  $counter++;
+}
+#print TEST Dumper(\@coordinates), "\n";
+#close TEST;
+
+#===================================== GENERATING LOCATIONSSHIFTED ARRAY =========================================================================
+my @locationsShifted = ();
+my $shiftFrame=0;
+my $coordinateFrame=0;
+my $coordinatesLen = scalar @coordinates;
+my $locationShiftsLen = scalar @locationshifts;
+my $shiftFrameLimit = $locationShiftsLen-1;
+my $isRev;
+my $oldShift = 0;
+$counter = 0;
+my ($cds_start, $cds_end, $isRev);
+for (my $coordinateFrame;$coordinateFrame<$coordinatesLen;$coordinateFrame++){
+    for (my $i=0;$i<3;$i++) {
+	if ($i == 0) {
+	    ($cds_start, $oldShift, $shiftFrame) = &calcCoordinates($shiftFrame, $coordinateFrame, $locationShiftsLen, $oldShift, $i, $shiftFrameLimit);
+	}        
+	elsif ($i == 1) {
+	    ($cds_end, $oldShift, $shiftFrame) = &calcCoordinates($shiftFrame, $coordinateFrame, $locationShiftsLen, $oldShift, $i, $shiftFrameLimit);
+	}
+	elsif ($i == 2) {
+	    $isRev = $coordinates[$coordinateFrame][$i];
+	}
+	$oldShift = $oldShift;
+	$shiftFrame = $shiftFrame;
+    }
+    $oldShift = $oldShift;
+    $shiftFrame = $shiftFrame;
+    push (@{$locationsShifted[$counter]}, ($cds_start, $cds_end, $isRev));
+    $counter++;
+}
+#print TEST Dumper(\@locationsShifted), "\n";
+#close TEST;
+
+#========================== CREATING REGION FILE FOR SAMTOOLS ===================================================================================                                                          
+open(REGION, ">$regionFile") or die "Cannot create a region file: $!";
+my $locationsShiftedLen = scalar @locationsShifted;
+my $adjustedLen = $locationsShiftedLen - 1;
+my @lengthArray = (0..$adjustedLen);
+for my $i (@lengthArray) {
+    print REGION "$sequenceId:$locationsShifted[$i][0]-$locationsShifted[$i][1]\t$locationsShifted[$i][2]\n";
+}
+close REGION;
+
+#========================== Generating locationsShiftedNonCodingLen Object ===========================================================================
+
+my @locationsShiftedNonCodingLen = ();
+my $noncodingLen=0;
+my $endingPosition=0;
+my $locationsShiftedLen = scalar @locationsShifted;
+my $locationShiftsLen = scalar @locationshifts;
+for (my $i=0;$i<$locationsShiftedLen;$i++){
+    $noncodingLen = $locationsShifted[$i][0] - $endingPosition - 1 + $noncodingLen;
+    $endingPosition = $locationsShifted[$i][1];
+    push (@{$locationsShiftedNonCodingLen[$i]}, ($locationsShifted[$i][0], $locationsShifted[$i][1], $noncodingLen));
+}
+#print TEST Dumper(\@locationsShiftedNonCodingLen), "\n";
+#close TEST;
+
+#======================================= GENERATING TRANSCRIPT LOCATION ARRAY  =========================================================================                                                    
+my @transcriptLocation = ();
+my $locationsShiftedNonCodingFrame=0;
+my $shiftFrame=0;
+my $locationsShiftedNonCodingFrameLen = scalar @locationsShiftedNonCodingLen;
+my $locationsShiftedNonCodingFrameLimit = $locationsShiftedNonCodingFrameLen - 1;
+my $locationShiftsLen = scalar @locationshifts;
+my $snpLocation;
+my $transcriptLocation;
+for (my $shiftFrame;$shiftFrame<$locationShiftsLen;$shiftFrame++){
+    $snpLocation = $locationshifts[$shiftFrame][0];
+    ($transcriptLocation, $locationsShiftedNonCodingFrame) = &getTranscriptLocation($snpLocation, $locationsShiftedNonCodingFrame, $locationsShiftedNonCodingFrameLimit, $shiftFrame);
+    push (@{$transcriptLocation[$shiftFrame]}, ($snpLocation, $transcriptLocation));
+}
+print TEST Dumper(\@transcriptLocation), "\n";
+close TEST; 
+
+#=================================== SUBROUTINES ==========================================================================================
+
+sub calcCoordinates {
+    my ($shiftFrame, $coordinateFrame, $locationshiftsLen, $oldShift, $i, $shiftFrameLimit) = @_;;
+    my $coordinate;
+    my $oldFrame;
+    if ($coordinates[$coordinateFrame][$i] < $locationshifts[$shiftFrame][0]) {
+	$coordinate = $oldShift + $coordinates[$coordinateFrame][$i];
+    }
+    elsif ($locationshifts[$shiftFrame][0] == $coordinates[$coordinateFrame][$i]) {
+        my $currentShift = $locationshifts[$shiftFrame][1];
+        if ($currentShift == 0) {
+	    $coordinate = $coordinates[$coordinateFrame][$i];
+        }
+        elsif ($i == 0) {
+	    $coordinate = $oldShift + $coordinates[$coordinateFrame][$i];
+        }
+        elsif ($i == 1 && $currentShift > 0) {
+	    $coordinate = $currentShift + $coordinates[$coordinateFrame][$i];     
+        }
+	elsif ($i == 1 && $currentShift < 0) {
+	    $coordinate = $oldShift + $coordinates[$coordinateFrame][$i];     
+        }
+    }
+    elsif ($coordinates[$coordinateFrame][$i] > $locationshifts[$shiftFrame][0] || $shiftFrame == $shiftFrameLimit) {
+	until ($locationshifts[$shiftFrame][0] >= $coordinates[$coordinateFrame][$i] || $shiftFrame == $shiftFrameLimit) {
+	    $oldShift = $locationshifts[$shiftFrame][1];
+	    $shiftFrame++;
+	}
+	if ($shiftFrame == $shiftFrameLimit && $coordinates[$coordinateFrame][$i] < $locationshifts[$shiftFrame][0]) {
+	    $coordinate = $coordinates[$coordinateFrame][$i] + $locationshifts[$shiftFrame-1][1];
+	}
+	elsif ($shiftFrame == $shiftFrameLimit && $coordinates[$coordinateFrame][$i] > $locationshifts[$shiftFrame][0]) {
+	    $coordinate = $coordinates[$coordinateFrame][$i] + $locationshifts[$shiftFrame][1];
+	}
+	elsif ($locationshifts[$shiftFrame][0] == $coordinates[$coordinateFrame][$i]) {
+	    if ($locationshifts[$shiftFrame][1] == 0) {
+                $coordinate = $coordinates[$coordinateFrame][$i];
+            }
+            elsif ($i == 0) {
+		$oldFrame = $shiftFrame - 1;
+                $coordinate = $locationshifts[$oldFrame][1] + $coordinates[$coordinateFrame][$i];
+            }
+            elsif ($i == 1 && $locationshifts[$shiftFrame][1] > 0) {
+                $coordinate = $locationshifts[$shiftFrame][1] + $coordinates[$coordinateFrame][$i];     
+            }
+	    elsif ($i == 1 && $locationshifts[$shiftFrame][1] < 0) {
+                $oldFrame = $shiftFrame - 1;
+                $coordinate = $locationshifts[$oldFrame][1] + $coordinates[$coordinateFrame][$i];     
+            }
+        }
+	else {
+	    $coordinate = $oldShift + $coordinates[$coordinateFrame][$i];
+	}
+    }
+    return ($coordinate, $oldShift, $shiftFrame);   
+}
+
+
+sub getTranscriptLocation {
+    my ($snpLocation, $locationsShiftedNonCodingFrame, $locationsShiftedNonCodingFrameLimit) = @_;
+    if ($locationsShiftedNonCodingFrame == $locationsShiftedNonCodingFrameLimit) {
+        if ($snpLocation >= $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][0] && $snpLocation <= $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][1]) {
+            $transcriptLocation = &calcTranscriptLocation($snpLocation, $shiftFrame, $locationsShiftedNonCodingFrame);
+        }
+        else {
+           $transcriptLocation = "NC";
+        }
+    }
+    elsif ($snpLocation >= $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][0] && $snpLocation <= $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][1]) {
+        $transcriptLocation = &calcTranscriptLocation($snpLocation, $shiftFrame, $locationsShiftedNonCodingFrame);
+    }
+    elsif ($snpLocation > $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][1] || $locationsShiftedNonCodingFrame == $locationsShiftedNonCodingFrameLimit) {
+        until ($snpLocation <= $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][1] || $locationsShiftedNonCodingFrame == $locationsShiftedNonCodingFrameLimit) {
+	    $locationsShiftedNonCodingFrame++;
+        }
+        if ($locationsShiftedNonCodingFrame == $locationsShiftedNonCodingFrameLimit) {
+            if ($snpLocation >= $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][0] && $snpLocation <= $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][1]) {
+		$transcriptLocation = &calcTranscriptLocation($snpLocation, $shiftFrame, $locationsShiftedNonCodingFrame);
+	    }
+	    else {
+		$transcriptLocation = "NC";
+	    }
+        }
+        elsif ($snpLocation >= $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][0]) {
+            $transcriptLocation = &calcTranscriptLocation($snpLocation, $shiftFrame, $locationsShiftedNonCodingFrame);
+        }
+        else {
+            $transcriptLocation = "NC";
+        }
+    }
+    else {
+        $transcriptLocation = "NC";
+    }
+    return ($transcriptLocation, $locationsShiftedNonCodingFrame);   
+}
+
+sub calcTranscriptLocation {
+    my ($snpLocation, $shiftFrame, $locationsShiftedNonCodingFrame) = @_;
+    my $transcriptLocation;
+    if ($shiftFrame = 0) {
+        $transcriptLocation = $snpLocation - $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][2] + 1;
+    }
+    else {
+        $transcriptLocation = $snpLocation - $locationsShiftedNonCodingLen[$locationsShiftedNonCodingFrame][2] + $locationshifts[$shiftFrame-1][1] + 1;
+    }
+    return $transcriptLocation;
+}
