863aeb56c35fe0731a24428f65de49719820ccd0
rdemko2332 <rdemko2332@gmail.com>
2023-01-25 13:37:35 -0500

    Placing variables in quotation marks to add pipe issue in db specs

--- modules/templates/processSeqVars.bash
+++ modules/templates/processSeqVars.bash
@@ -9,10 +9,10 @@ processSequenceVariationsNew.pl \
   --new_sample_file snpFile.tsv \
   --cache_file cache.txt \
   --undone_strains_file undoneStrains.txt \
-  --transcript_extdb_spec $genomeExtDbRlsSpec \
+  --transcript_extdb_spec "$genomeExtDbRlsSpec" \
   --organism_abbrev $organism_abbrev \
   --reference_strain $reference_strain  \
-  --extdb_spec $extdb_spec \
+  --extdb_spec "$extdb_spec" \
   --varscan_directory varscan_directory/ \
   --gusConfigFile gusConfig.txt \
   --genome genome.fa \
