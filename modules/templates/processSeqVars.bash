0fa6048b0df38e73fe1cead23d8f9f57e1e224da
rdemko2332 <rdemko2332@gmail.com>
2023-01-27 14:42:40 -0500

    Files to variables

--- modules/templates/processSeqVars.bash
+++ modules/templates/processSeqVars.bash
@@ -5,23 +5,14 @@ set -euo pipefail
 cp $consensusFasta unzipped.fa.gz;
 gunzip unzipped.fa.gz;
 
-cp $snpFile snp.tsv
-cp $cacheFile cf.txt
-cp $undoneStrainsFile us.txt
-mkdir varCons
-cp $varscanDir/* varCons
-cp $genomeFasta genome.fasta
-cp $indelFile if.tsv
-cp $gtfFile gtf.gtf
-
 perl /usr/bin/processSequenceVariationsNew.pl \
-  --new_sample_file snp.tsv \
-  --cache_file cf.txt \
-  --undone_strains_file us.txt \
+  --new_sample_file $snpFile \
+  --cache_file $cacheFile \
+  --undone_strains_file $undoneStrainsFile \
   --organism_abbrev $organism_abbrev \
   --reference_strain $reference_strain  \
-  --varscan_directory ./varCons \
-  --genome genome.fasta \
+  --varscan_directory $varscanDir \
+  --genome $genomeFasta \
   --consensus unzipped.fa \
-  --indelFile if.tsv \
-  --gtfFile gtf.gtf
+  --indelFile $indelFile \
+  --gtfFile $gtfFile
