7bcee08225afaabe8046b9b361eae0117055689c
rdemko2332 <rdemko2332@gmail.com>
2023-05-08 09:39:10 -0400

    Removing % sign from result vcfs

--- modules/templates/mergeVcfsProcessSingle.bash
+++ modules/templates/mergeVcfsProcessSingle.bash
@@ -5,9 +5,15 @@ set -euo pipefail
 if [ $vcfCount -gt 1 ]; then
 
     bcftools merge -o result.vcf.gz -O z *.vcf.gz
+    gunzip result.vcf.gz
+    sed -i 's/\%//g' result.vcf
+    gzip result.vcf
     
 else
 
     cp *.vcf.gz result.vcf.gz
+    gunzip result.vcf.gz
+    sed -i 's/\%//g' result.vcf
+    gzip result.vcf
 
 fi
