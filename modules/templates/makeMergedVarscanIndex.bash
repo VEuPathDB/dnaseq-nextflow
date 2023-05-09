b708809f851bb652a8fa52b40f7d154dc83977c7
rdemko2332 <rdemko2332@gmail.com>
2023-05-09 13:52:32 -0400

    Resolving bzip error

--- modules/templates/makeMergedVarscanIndex.bash
+++ modules/templates/makeMergedVarscanIndex.bash
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+gunzip $resultVcfGz
+sed -i 's/\\%//g' *.vcf
+bgzip *.vcf
 tabix -fp vcf $resultVcfGz
--- modules/templates/mergeVcfsProcessSingle.bash
+++ modules/templates/mergeVcfsProcessSingle.bash
@@ -5,15 +5,9 @@ set -euo pipefail
 if [ $vcfCount -gt 1 ]; then
 
     bcftools merge -o result.vcf.gz -O z *.vcf.gz
-    gunzip result.vcf.gz
-    sed -i 's/\\%//g' result.vcf
-    bgzip result.vcf
-    
+      
 else
 
     cp *.vcf.gz result.vcf.gz
-    gunzip result.vcf.gz
-    sed -i 's/\\%//g' result.vcf
-    bgzip result.vcf
-
+  
 fi
