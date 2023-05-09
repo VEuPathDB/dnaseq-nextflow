39cace75233ed61d572a1aab73e1e9c100e6ff38
rdemko2332 <rdemko2332@gmail.com>
2023-05-09 13:14:28 -0400

    gzip to bgzip

--- modules/templates/mergeVcfsProcessSingle.bash
+++ modules/templates/mergeVcfsProcessSingle.bash
@@ -7,13 +7,13 @@ if [ $vcfCount -gt 1 ]; then
     bcftools merge -o result.vcf.gz -O z *.vcf.gz
     gunzip result.vcf.gz
     sed -i 's/\\%//g' result.vcf
-    gzip result.vcf
+    bgzip result.vcf
     
 else
 
     cp *.vcf.gz result.vcf.gz
     gunzip result.vcf.gz
     sed -i 's/\\%//g' result.vcf
-    gzip result.vcf
+    bgzip result.vcf
 
 fi
