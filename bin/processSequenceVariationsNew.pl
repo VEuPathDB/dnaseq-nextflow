9d8d209dfb3f1c8bfcb71a67b81de2da688611c9
rdemko2332 <rdemko2332@gmail.com>
2023-06-21 10:45:39 -0400

    Reverting changes to check

--- bin/processSequenceVariationsNew.pl
+++ bin/processSequenceVariationsNew.pl
@@ -1206,14 +1206,12 @@ sub makeAlleleFeatureFromVariations {
       }
       $distinctStrainCount = scalar keys %strains;
       $avg_read_percent = $avg_read_percent / $count;
-      my $rounded_avg_read_percent = sprintf("%.2f", $avg_read_percent);
       $avg_coverage = $avg_coverage / $count;
-      my $rounded_avg_coverage = sprintf("%.2f", $avg_coverage);
       my $all = { "allele" => $allele,
                   "distinct_strain_count" => $distinctStrainCount,
 	          "allele_count" => $count,
-	          "average_coverage" => $rounded_avg_coverage,
-	          "average_read_percent" => $rounded_avg_read_percent
+	          "average_coverage" => $avg_coverage,
+	          "average_read_percent" => $avg_read_percent
                 };
       push @$alleles, $all;
   }
