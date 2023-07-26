5db310e4f774c5fffb6bb5f39838e8552dfcd2de
rdemko2332 <rdemko2332@gmail.com>
2023-07-26 14:56:55 -0400

    Updating add features script

--- bin/addFeatureIdsToVariation.pl
+++ bin/addFeatureIdsToVariation.pl
@@ -8,11 +8,11 @@ use GUS::ObjRelP::DbiDatabase;
 use GUS::Supported::GusConfig;
 
 # get parameter values
-my ($variationFile, $gusConfigFile, $extDbRlsSpec);
+my ($variationFile, $gusConfigFile);
 
 &GetOptions("variationFile=s" => \$variationFile,
             "gusConfig=s" => \$gusConfigFile
-           );
+    );
 
 my $gusConfig = GUS::Supported::GusConfig->new($gusConfigFile);
 
@@ -32,30 +32,31 @@ open($outputFile, "> ./variationFeatureFinal.dat") or die "Cannot open file vari
 open(my $data, '<', $variationFile) || die "Could not open file $variationFile: $!";
 while (my $line = <$data>) {
     chomp $line;
-    my ($location, $sourceId, $transcriptId, $referenceStrain, $referenceNa, $referenceAa, $hasNonsynonymousAllele, $majorAllele, $minorAllele, $majorAlleleCount, $minorAlleleCount, $majorProduct, $minorProduct, $distinctStrainCount, $distinctAlleleCount, $hasCodingMutation, $totalAlleleCount, $hasStopCodon, $refCodon) = split(/\t/, $line);
+    my ($location, $geneNaFeatureId, $sourceId, $referenceStrain, $referenceNa, $referenceAa, $hasNonsynonymousAllele, $majorAllele, $minorAllele, $majorAlleleCount, $minorAlleleCount, $majorProduct, $minorProduct, $distinctStrainCount, $distinctAlleleCount, $hasCodingMutation, $totalAlleleCount, $hasStopCodon, $refCodon, $referenceAAFull) = split(/\t/, $line);
+    my $naFeatureId = &queryNaFeatureId($dbh, $geneNaFeatureId);
     my $naSequenceId = &queryNaSequenceId($dbh, $sourceId);
-    my $naFeatureId = &queryNaFeatureId($dbh, $transcriptId);
-    print $outputFile "$location\t$naFeatureId\t$naSequenceId\t$source_id\t$referenceStrain\t$referenceNa\t$referenceAa\t$hasNonsynonymousAllele\t$majorAllele\t$minorAllele\t$majorAlleleCount\t$minorAlleleCount\t$majorProduct\t$minorProduct\t$distinctStrainCount\t$distinctAlleleCount\t$hasCodingMutation\t$totalAlleleCount\t$hasStopCodon\t$refCodon\n";
+    print $outputFile "$location\t$naFeatureId\t$naSequenceId\t$sourceId\t$referenceStrain\t$referenceNa\t$hasNonsynonymousAllele\t$majorAllele\t$minorAllele\t$majorAlleleCount\t$minorAlleleCount\t$majorProduct\t$minorProduct\t$distinctStrainCount\t$distinctAlleleCount\t$hasCodingMutation\t$totalAlleleCount\t$hasStopCodon\t$refCodon\n";
 }
 
 close $outputFile;
 close $data;
 
 sub queryNaFeatureId {
-    my ($dbh, $transcriptId) = @_;
+    my ($dbh, $geneNaFeatureId) = @_;
+   
+    my $nafeatureid;
+ 
+    if ($geneNaFeatureId) {
+        my $sql = "select t.na_feature_id from dots.transcript t where t.source_id = '$geneNaFeatureId'";
 
-    my $sql = "select na_feature_id 
-    from dots.transcript 
-    where source_id = '$transcriptId'";
+        my $sh = $dbh->prepare($sql);
+        $sh->execute();
 
-    my $sh = $dbh->prepare($sql);
-    $sh->execute();
+        $nafeatureid = $sh->fetchrow_array();
 
-    my ($nafeatureid) = $sh->fetchrow_array();
-
-    $sh->finish();
-    die "Could not find na_feature_id for source_id: $sourceid" unless($nafeatureid);
-
+        $sh->finish();
+        die "Could not find na_feature_id for source_id: $geneNaFeatureId" unless($nafeatureid);
+    }
     return $nafeatureid;
 }
 
@@ -63,9 +64,7 @@ sub queryNaFeatureId {
 sub queryNaSequenceId {
     my ($dbh, $sourceid) = @_;
 
-    my $sql = "select na_sequence_id 
-    from dots.nasequence 
-    where source_id = '$sourceid'"; 
+    my $sql = "select s.na_sequence_id from dots.naSequence s where s.source_id = '$sourceid'";
 
     my $sh = $dbh->prepare($sql);
     $sh->execute();
