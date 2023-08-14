e8acab6a3916a64eb933882d8a20b5886b0b2251
rdemko2332 <rdemko2332@gmail.com>
2023-08-14 10:36:58 -0400

    Forcing gzip

--- modules/templates/snpEff.bash
+++ modules/templates/snpEff.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 mkdir genome
 mv $genesGtf genome/genes.gtf
 mv $sequencesFa genome/sequences.fa
-gzip genome/sequences.fa
+gzip -f genome/sequences.fa
 cp /usr/bin/snpEff/snpEff.config .
 java -jar /usr/bin/snpEff/snpEff.jar build -gtf22 -noCheckCds -noCheckProtein -v genome
 java -Xmx4g -jar /usr/bin/snpEff/snpEff.jar genome $mergedVcf > merged.ann.vcf
